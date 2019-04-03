a517b15b1aed4e2c8fc40a82d1e10f930382c2f3
Nitai Shavit <nitai230@gmail.com>
2019-04-03 15:08:03 +0300

    final test

--- src/index.js
+++ src/index.js
@@ -5,15 +5,13 @@ import {createStore, applyMiddleware} from 'redux'
 import {combineReducers} from 'redux'
 import {reducer as formReducer} from 'redux-form'
 import {movieReducer} from './store/MovieReducer'
-import { composeWithDevTools } from 'remote-redux-devtools';
 import thunk from 'redux-thunk'
 import {Provider} from 'react-redux'
 
-const composeEnhancers = composeWithDevTools({ realtime: true });
 
 const store = createStore(
     combineReducers({movieReducer, form: formReducer}),
-    composeEnhancers(applyMiddleware(thunk))
+   applyMiddleware(thunk)
 )
 
 ReactDOM.render(<Provider store={store}><App/></Provider>, document.getElementById('root'));
